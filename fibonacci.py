dd884e94d730e5a39711aabecd10506718cbf25b
Mat Bakarich <mbakarich1@mail.pima.edu>
2025-03-30 20:57:14 -0700

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,8 @@
 def fibonacci(n):
+    """Calculate the nth Fibonacci number and track the total number of function calls.
+       Returns a tuple where the first element is the Fibonacci number and the second element is
+       the total number of function calls made to fibonacci().
+    """
    
     if n in (0, 1): # base cases
         return n, 1
@@ -9,16 +13,12 @@ def fibonacci(n):
         return f1 + f2, n1 + n2 + 1
                
     
-#printfunction calls
-
+# call Fibonacci and store the result.
 fib_10 = fibonacci(10)
 fib_20 = fibonacci(20)
 fib_30 = fibonacci(30)
 
+# print Fibonacci number and number of function calls.
 print(f"fibonacci(10) = {fib_10}")
 print(f"fibonacci(20) = {fib_20}")
 print(f"fibonacci(30) = {fib_30}")
-
-fib_3 = fibonacci(3)
-print(fib_3)
-
